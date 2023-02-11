66672eedd06c6cc3d8181f8c9a7334ec85a6465d
shweta <shweta.kamboj1992@gmail.com>
2023-02-11 16:56:36 +0530

    netlify changes

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const serverless = require("serverless-http");
 const format = require("date-format");
+const router = express.Router();
 
 const app = express()
 
@@ -21,3 +23,6 @@ app.get('/v1/instagram', (req, res) => {
 app.listen(PORT, () => {
     console.log(`server is running at ${PORT}`)
 })
+
+app.use('/.netlify/functions/index', router)
+module.exports.handler = serverless(app)
